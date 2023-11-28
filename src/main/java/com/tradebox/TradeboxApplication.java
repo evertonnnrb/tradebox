32941f95a1bd3849876011cafd77a8f10476738c
Everton Ribeiro <everton.nnrb@gmail.com>
2023-11-28 05:01:15 -0400

    first commit

---
+++ src/main/java/com/tradebox/TradeboxApplication.java
@@ -0,0 +1,13 @@
+package com.tradebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeboxApplication.class, args);
+	}
+
+}
